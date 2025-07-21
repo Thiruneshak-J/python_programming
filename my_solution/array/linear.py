5f6c0a59006804424bb9e9be703bd0646381383a
Thiru™ <saritha02.03.2005@gmail.com>
2025-07-21 17:59:19 +0530

    searching

--- my_solution/array/linear.py
+++ my_solution/array/linear.py
@@ -1,1 +1,9 @@
-n=int(input("enter the size:"))
+import array as ar
+n1=list(map(int,input("enter the number:(by comma)").split(",")))
+n=ar.array('i',n1)
+print("you enterd element are ",n)
+e=int(input("enter the element:"))
+for i in range(0,len(n)):
+    if n[i]==e:
+        print("the index of your element is : ",i)
+        print("the position is :",i+1)
